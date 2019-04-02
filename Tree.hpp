a585146c145a67f75d4405cef1f9a5ab7f105e87
naamahartuv <44947865+naamahartuv@users.noreply.github.com>
2019-04-02 20:41:50 +0300

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
-
-
+//this struct represents a node in binary tree
 struct node{
     private:
     int value;
@@ -13,7 +11,10 @@ struct node{
     public: 
     node(int v){
         value = v;
+        right = NULL;
+        left = NULL;
     }
+    
     int getValue(){
         return value;
     }
@@ -24,6 +25,10 @@ struct node{
     
     struct node* getRight(){
         return right;
+    }
+
+    void setValue(int v){
+        value = v;
     }
 
     void setLeft(struct node* temp){
@@ -38,6 +43,7 @@ struct node{
 
 namespace ariel{
 
+    //this class represents a binary tree
     class Tree{
 
         private:
@@ -47,29 +53,26 @@ namespace ariel{
         void insert(int i, struct node* node);
         struct node* smallOrLarge(int i, struct node* node);
         void print(struct node* node);
-
-        //add exceptions!!!!!!!!!!!!!!!!!-insert, parent, left, right
-        //change insert to return tree&
+        void deleteTree( node* nodee);
+        struct node* remove( int i ,struct node* node);
+        bool isRight(struct node* node, struct node* parent);
+        struct node* findMin(struct node* node);
 
         public:
         Tree(){ 
             head = NULL;
             treeSize = 0;
-            }
+        }
+        ~Tree();
         Tree& insert(int i);
         void remove(int i);
-        int size(); //
+        int size(); 
         bool contains(int i);
-        int root(); //
+        int root(); 
         int parent(int i);
         int left(int i);
         int right(int i);
         void print();
-
-
-
-
-
 
 
 
